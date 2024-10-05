5f9a2dadfe0e80012d40e7147a3762ede8c694a3
Pengxiang Huang <huangpengxiang70@gmail.com>
2024-10-05 09:39:40 -0500

    fix a bug in dashboard

--- nvim/lua/dashboard.lua
+++ nvim/lua/dashboard.lua
@@ -27,7 +27,7 @@ dashboard.section.buttons.val = {
     dashboard.button( "n", "  > New file" , ":ene <BAR> startinsert <CR>"),
     dashboard.button( "f", "  > Find file", ":cd $HOME/ | Telescope find_files<CR>"),
     dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
-    dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | pwd<CR>"),
+    dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | :Neotree<CR>"),
     dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
 }
 
